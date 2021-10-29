a66f17226663f9783cb857715e049769a6b2f4df
Sebastian Ankargren <sebastiana@spotify.com>
2021-10-29 11:10:56 +0200

    adding test for univariate case

--- tests/frequentist/test_ztest_linreg.py
+++ tests/frequentist/test_ztest_linreg.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 import spotify_confidence
+from spotify_confidence.analysis.constants import REGRESSION_PARAM
 
 
 class Test(object):
@@ -11,19 +12,22 @@ class Test(object):
         d = np.random.randint(2, size=n)
         x = np.random.standard_normal(size=n)
         y = 0.5 * d + 0.5 * x + np.random.standard_normal(size=n)
-        country = np.random.choice(["us", "se"], n)
-        data = pd.DataFrame({"variation_name": list(map(str, d)), "y": y, "x": x, "country": country})
+        data = pd.DataFrame({"variation_name": list(map(str, d)), "y": y, "x": x})
         data = (
             data.assign(xy=y * x)
             .assign(x2=x ** 2)
             .assign(y2=y ** 2)
-            .groupby(["variation_name", "country"])
+            .groupby(["variation_name"])
             .agg({"y": ["sum", "count"], "y2": "sum", "x": "sum", "x2": "sum", "xy": "sum"})
             .reset_index()
         )
 
         data.columns = data.columns.map("_".join).str.strip("_")
         data.assign(**{"metric_name": "metricA"})
+        self.n = n
+        self.x = x
+        self.y = y
+        self.d = d
         self.data = data
 
 
@@ -32,7 +36,7 @@ class Test(object):
             numerator_column="y_sum",
             numerator_sum_squares_column="y2_sum",
             denominator_column="y_count",
-            categorical_group_columns=["variation_name", "country"],
+            categorical_group_columns=["variation_name"],
             feature_column="x_sum",
             feature_sum_squares_column="x2_sum",
             feature_cross_sum_column="xy_sum",
@@ -44,3 +48,18 @@ class Test(object):
         summary_df = self.test.summary(verbose=True)
         print(summary_df)
         assert len(summary_df) == len(self.data)
+
+    def test_parameters_univariate(self):
+        def linreg(X, y):
+            return np.matmul(np.linalg.inv(np.matmul(np.transpose(X), X)), np.matmul(np.transpose(X), y))
+        summary_df = self.test.summary(verbose=True)
+        X = np.ones((self.n, 2))
+        X[:, 1] = self.x
+        b0 = linreg(X, self.y)
+        y_adj = self.y - np.matmul(X, b0)
+        X[:, 1] = self.d
+        b1 = linreg(X, y_adj)
+        assert np.allclose(b0[1], summary_df[REGRESSION_PARAM][0], rtol=0.0001)
+
+        diff = self.test.difference(level_1='0', level_2='1')
+        assert np.allclose(b1[1], diff['difference'])
