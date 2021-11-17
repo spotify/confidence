- Now using p*(1-p) for binary variance - Fix numerical inaccuracy in sequential bounds solver - Introduce slow sequential bounds test to benchmark performance aggainst

spotify_confidence/analysis/frequentist/confidence_computers/t_test_computer.py
@@ -34,9 +34,15 @@ class TTestComputer(object):
         return row[self._numerator] / row[self._denominator]
 
     def _variance(self, row: Series) -> float:
-        variance = (row[self._numerator_sumsq] - np.power(row[self._numerator], 2) / row[self._denominator]) / (
-            row[self._denominator] - 1
-        )
+        binary = row[self._numerator_sumsq] == row[self._numerator]
+        if binary:
+            # This equals row[POINT_ESTIMATE]*(1-row[POINT_ESTIMATE]) when the data is binary,
+            # and also gives a robust fallback in case it's not
+            variance = row[self._numerator_sumsq] / row[self._denominator] - row[POINT_ESTIMATE] ** 2
+        else:
+            variance = (row[self._numerator_sumsq] - np.power(row[self._numerator], 2) / row[self._denominator]) / (
+                row[self._denominator] - 1
+            )
         if variance < 0:
             raise ValueError("Computed variance is negative. " "Please check your inputs.")
         return variance

spotify_confidence/analysis/frequentist/confidence_computers/z_test_computer.py
@@ -1,6 +1,7 @@
 from typing import Tuple, Union
 
 import numpy as np
+import pandas as pd
 from pandas import DataFrame, Series
 from scipy import optimize
 from scipy import stats as st
@@ -38,8 +39,8 @@ from spotify_confidence.analysis.constants import (
 from spotify_confidence.analysis.frequentist.sequential_bound_solver import bounds
 
 
-def sequential_bounds(t: np.array, alpha: float, sides: int):
-    return bounds(t, alpha, rho=2, ztrun=8, sides=sides, max_nints=1000)
+def sequential_bounds(t: np.array, alpha: float, sides: int, state: pd.DataFrame = None):
+    return bounds(t, alpha, rho=2, ztrun=8, sides=sides, max_nints=1000, state=state)
 
 
 class ZTestComputer(object):
@@ -56,9 +57,15 @@ class ZTestComputer(object):
         return row[self._numerator] / row[self._denominator]
 
     def _variance(self, row: Series) -> float:
-        variance = (row[self._numerator_sumsq] - np.power(row[self._numerator], 2) / row[self._denominator]) / (
-            row[self._denominator] - 1
-        )
+        binary = row[self._numerator_sumsq] == row[self._numerator]
+        if binary:
+            # This equals row[POINT_ESTIMATE]*(1-row[POINT_ESTIMATE]) when the data is binary,
+            # and also gives a robust fallback in case it's not
+            variance = row[self._numerator_sumsq] / row[self._denominator] - row[POINT_ESTIMATE] ** 2
+        else:
+            variance = (row[self._numerator_sumsq] - np.power(row[self._numerator], 2) / row[self._denominator]) / (
+                row[self._denominator] - 1
+            )
         if variance < 0:
             raise ValueError("Computed variance is negative. " "Please check your inputs.")
         return variance

spotify_confidence/analysis/frequentist/sequential_bound_solver.py
@@ -38,11 +38,10 @@ def _qp(xq: float, last: float, nints: int, yam1: float, ybm1: float, stdv: floa
 def _bsearch(
     last: np.array,
     nints: int,
-    i: float,
     pd: float,
     stdv: float,
-    ya: np.array,
-    yb: np.array,
+    ya: float,
+    yb: float,
 ) -> np.array:
     """
     Note: function signature slightly modified in comparison to R implementation (which takes complete nints
@@ -51,8 +50,8 @@ def _bsearch(
     max_iter = 50
     tol = 1e-7
     de = 10
-    uppr = yb[i - 1]
-    q = _qp(uppr, last, nints, ya[i - 1], yb[i - 1], stdv)
+    uppr = yb
+    q = _qp(uppr, last, nints, ya, yb, stdv)
     while abs(q - pd) > tol:
         de = de / 10
         temp = 1 if q > (pd + tol) else 0
@@ -60,7 +59,7 @@ def _bsearch(
         j = 1
         while j <= max_iter:
             uppr = uppr + incr * de
-            q = _qp(uppr, last, nints, ya[i - 1], yb[i - 1], stdv)
+            q = _qp(uppr, last, nints, ya, yb, stdv)
             if abs(q - pd) > tol and j >= max_iter:
                 break
             elif (incr == 1 and q <= (pd + tol)) or (incr == -1 and q >= (pd - tol)):
@@ -75,19 +74,14 @@ _NORM_CONSTANT = 1 / np.sqrt(2 * np.pi)
 
 
 def _fast_norm_pdf_prescaled(x: np.array, scale):
-    norm_constant2 = 1 / (scale * np.sqrt(2 * np.pi))
+    norm_constant2 = _NORM_CONSTANT / scale
     pdf_val = norm_constant2 * np.exp(-0.5 * np.power(x, 2))
     return pdf_val
 
 
 def _fcab(last: np.array, nints: int, yam1: float, h: float, x: np.array, stdv: float):
-    nints = int(nints)
-    x_tiled = np.tile(x, nints + 1)
-    x_reshaped = x_tiled.reshape(nints + 1, len(x))
-    lin_calc_transposed = np.transpose(
-        np.tile((h * np.linspace(0, nints, nints + 1) + yam1), len(x)).reshape(len(x), nints + 1)
-    )
-    scaled_x = (lin_calc_transposed - x_reshaped) / stdv
+    X, Y = np.meshgrid(x / stdv, (h * np.linspace(0, nints, nints + 1) + yam1) / stdv)
+    scaled_x = Y - X
     pdf_prescaled = _fast_norm_pdf_prescaled(scaled_x, stdv)
     last_transposed = np.transpose(np.tile(last, len(x)).reshape(len(x), nints + 1))
 
@@ -172,7 +166,7 @@ def landem(
     zninf = -ztrun
     tol = 1e-7
 
-    t2 = t  # ldbounds:::bounds() rescales t2=t/t.max() by default. We omit this because impact on bounds unclear
+    # t2 = t  # ldbounds:::bounds() rescales t2=t/t.max() by default. We omit this because impact on bounds unclear
 
     if df.isnull().all().all():
         # start at index 0 if df was not yet initialized
@@ -183,11 +177,13 @@ def landem(
         start = zb_null_arr[0][0] - 1 if len(zb_null_arr[0]) > 0 else len(df) - 1
 
     rangestart = start + 1
-    for j in range(start, len(t)):
-        df.at[j, "stdv"] = np.sqrt(t2[j]) if j == 0 else np.sqrt(t2[j] - t2[j - 1])
+    if start == 0:
+        df.loc[0, "stdv"] = np.sqrt(t[0])
+
+    df.loc[start + 1 : len(t), "stdv"] = np.sqrt(t[start + 1 : len(t)] - t[start : len(t) - 1])
 
     df["pe"], df["pd"] = _alphas(alpha, phi, t)
-    df.loc[start:, "sdproc"] = np.sqrt(t2[start:])
+    df.loc[start:, "sdproc"] = np.sqrt(t[start:])
     df.loc[start:, "information_ratio"] = t[start:]
 
     if df.isnull().all(axis=0)[0]:
@@ -216,17 +212,18 @@ def landem(
 
     if len(t) >= 2:
         for i in range(rangestart, len(t)):
-            if t[i] - df["information_ratio"][i - 1] == 0:
+            if df["information_ratio"][i] - df["information_ratio"][i - 1] <= 1e-5:
                 # If information ratio difference between time steps is 0, re-use result calculated for the previous
                 # time step. Normally, it means that no data was added. We have to catch this case because nints
-                # becomes float("inf") and makes the procedure crash.
+                # becomes float("inf") and makes the procedure crash. We check against 10e-6 instead of against 0
+                # because an almost-zero information gain can cause pretty big numerical inaccuracy in practice.
                 df.iloc[i] = df.iloc[i - 1]
                 continue
 
-            if df.at[i, "pd"] < 0 or df.at[i, "pd"] > 1:
-                # Possible error in spending function.  May be due to truncation.
-                df.at[i, "pd"] = min(1, df.at[i, "pd"])
-                df.at[i, "pd"] = max(0, df.at[i, "pd"])
+            # Possible error in spending function.  May be due to truncation.
+            if df.at[i, "pd"] != 1.0:
+                df.at[i, "pd"] = df.at[i, "pe"] - df.at[i - 1, "pe"]
+            df.at[i, "pd"] = df.at[i, "pd"].clip(0, 1)
 
             if df.at[i, "pd"] < tol:
                 df.at[i, "zb"] = -zninf
@@ -241,22 +238,20 @@ def landem(
                         df.at[i, "stdv"],
                     )
                     df.at[i, "pe"] = df.at[i, "pd"] + df.at[i - 1, "pe"]
-                    if i < len(t) - 1:
-                        df.at[i + 1, "pd"] = df.at[i + 1, "pe"] - df.at[i, "pe"]
+
                 df.at[i, "yb"] = df.at[i, "zb"] * df.at[i, "sdproc"]
             elif df.at[i, "pd"] == 1.0:
                 df.at[i, "zb"] = 0.0
-                df.at[i, "zb"] = 0.0
+                df.at[i, "yb"] = 0.0
             elif tol <= df.at[i, "pd"] < 1:
 
                 df.at[i, "yb"] = _bsearch(
                     last_fcab,
                     int(df.loc[i - 1]["nints"]),  # differs from R because we modified signature of bsearch
-                    i,
                     df.at[i, "pd"],
                     df.at[i, "stdv"],
-                    df["ya"],
-                    df["yb"],
+                    df.at[i - 1, "ya"],
+                    df.at[i - 1, "yb"],
                 )
 
                 df.at[i, "zb"] = df.at[i, "yb"] / df.at[i, "sdproc"]
@@ -272,8 +267,7 @@ def landem(
                         df.at[i, "stdv"],
                     )
                     df.at[i, "pe"] = df.at[i, "pd"] + df.at[i - 1, "pe"]
-                    if i < len(t) - 1:
-                        df.at[i + 1, "pd"] = df.at[i + 1, "pe"] - df.at[i, "pe"]
+
                 df.at[i, "yb"] = df.at[i, "zb"] * df.at[i, "sdproc"]
 
             # in landem.R, the following two statements are in side==1 if clause
@@ -292,7 +286,9 @@ def landem(
                     df.at[i, "yb"],
                     int(df.at[i, "nints"] + 1),
                 )
-                last_fcab = _fcab(last_fcab, df.at[i - 1, "nints"], df.at[i - 1, "ya"], hlast, x, df.at[i, "stdv"])
+                last_fcab = _fcab(
+                    last_fcab, int(df.at[i - 1, "nints"]), df.at[i - 1, "ya"], hlast, x, df.at[i, "stdv"]
+                )
     return df, ComputationState(df, last_fcab)
 
 
@@ -339,7 +335,7 @@ def bounds(
     def get_input_str():
         return (
             f"input params: t={t}, alpha={alpha}, sides={sides}, rho={rho}, ztrun={ztrun},"
-            f"state_df={state.df.to_json()}, state_fcab={state.last_fcab}"
+            f"state_df={state.df.to_json()}, state_fcab={state.last_fcab}, max_nints={max_nints}"
         )
 
     if any(t == 0.0):
@@ -357,7 +353,12 @@ def bounds(
     df_result, new_state = landem(t, alph, rho, ztrun, state, max_nints)
 
     # guardrail check
-    if norm.ppf(1 - alph) > df_result["zb"].values[-1]:
-        raise Exception(f"Last bound is less conservative than fixed horizon bound, {get_input_str()}")
+    fixed_horizon_bound = norm.ppf(1 - alph)
+    last_sequential_bound = df_result["zb"].values[-1]
+    if fixed_horizon_bound > last_sequential_bound:
+        raise Exception(
+            f"Last bound ({last_sequential_bound}) is less conservative than fixed horizon bound "
+            f"({fixed_horizon_bound}), {get_input_str()} "
+        )
 
     return CalculationResult(df_result, new_state)

tests/frequentist/test_ttest.py
@@ -852,10 +852,10 @@ class TestCategoricalBinomialData(object):
         assert np.array_equal(summary.country, np.array(["us", "us", "us", "gb", "gb", "gb"]))
         assert np.array_equal(summary.point_estimate, self.data.success / self.data.total)
         assert np.allclose(
-            summary["ci_lower"], np.array([0.464638, 0.307962, 0.645285, 0.400289, 0.030591, 0.590355]), rtol=1e-3
+            summary["ci_lower"], np.array([0.464653, 0.308424, 0.645293, 0.400789, 0.049548, 0.590610]), rtol=1e-3
         )
         assert np.allclose(
-            summary["ci_upper"], np.array([0.526443, 0.499731, 0.692931, 0.599711, 0.769409, 0.742978]), rtol=1e-3
+            summary["ci_upper"], np.array([0.526427, 0.499269, 0.692923, 0.599211, 0.750452, 0.742723]), rtol=1e-3
         )
 
     def test_multiple_difference(self):
@@ -865,38 +865,28 @@ class TestCategoricalBinomialData(object):
         diff = self.test.multiple_difference(("us", "control"), level_as_reference=False)
         assert np.allclose(
             diff["adjusted p-value"],
-            np.array([1.00000000e00, 8.47296785e-01, 1.60789003e-04, 3.68641436e-01, 2.53362343e-06]),
+            np.array([1e00, 8.36850129e-01, 1.49172324e-04, 3.62369184e-01, 2.27154393e-06]),
             rtol=1e-3,
         )
         assert np.allclose(
             diff["p-value"],
-            np.array([9.82400227e-01, 1.69459357e-01, 3.21578005e-05, 7.37282872e-02, 5.06724687e-07]),
+            np.array([9.81516149e-01, 1.67370026e-01, 2.98344648e-05, 7.24738369e-02, 4.54308787e-07]),
             rtol=1e-3,
         )
         assert np.allclose(
-            diff["adjusted ci_lower"],
-            np.array([-0.52870939, -0.27749219, -0.42362922, -0.22468195, -0.39580312]),
-            rtol=1e-3,
+            diff["adjusted ci_lower"], np.array([-0.501747, -0.276600, -0.422946, -0.224093, -0.395207]), rtol=1e-3
         )
         assert np.allclose(
-            diff["adjusted ci_upper"],
-            np.array([0.5364017, 0.0851845, -0.10201181, 0.04129398, -0.13472028]),
-            rtol=1e-3,
+            diff["adjusted ci_upper"], np.array([0.509440, 0.084292, -0.102695, 0.040705, -0.135317]), rtol=1e-3
         )
 
         diff = self.test.multiple_difference("test", groupby="country", level_as_reference=False)
+        assert np.allclose(diff["adjusted p-value"], np.array([1, 0.035594, 0.289895, 0.0]), rtol=1e-3)
+        assert np.allclose(diff["p-value"], np.array([0.55155672, 0.00889848, 0.07247384, 0.0]), rtol=1e-3)
         assert np.allclose(
-            diff["adjusted p-value"], np.array([1.000000e00, 3.679225e-02, 2.949131e-01, 0.0]), rtol=1e-3
+            diff["adjusted ci_lower"], np.array([-0.385571, -0.325682, -0.036587, -0.223262]), rtol=1e-3
         )
-        assert np.allclose(
-            diff["p-value"], np.array([5.70407366e-01, 9.19806264e-03, 7.37282872e-02, 0.000]), rtol=1e-3
-        )
-        assert np.allclose(
-            diff["adjusted ci_lower"], np.array([-0.41130848, -0.32638507, -0.03715783, -0.22328344]), rtol=1e-3
-        )
-        assert np.allclose(
-            diff["adjusted ci_upper"], np.array([0.61130848, -0.00694826, 0.2205458, -0.123852]), rtol=1e-3
-        )
+        assert np.allclose(diff["adjusted ci_upper"], np.array([0.585571, -0.0076513, 0.219975, -0.123874]), rtol=1e-3)
 
 
 class TestWithNims(object):

tests/frequentist/test_ztest.py
@@ -741,12 +741,12 @@ class TestPoweredEffectBinary(object):
         assert powered_effect[REQUIRED_SAMPLE_SIZE][3] == float("inf")
         assert powered_effect[REQUIRED_SAMPLE_SIZE][4] == float("inf")
         assert powered_effect[REQUIRED_SAMPLE_SIZE][5] == float("inf")
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][6], 261852, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][7], 363685, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][8], 327800, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][9], 261852, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][10], 363685, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][11], 327800, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][6], 260541, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][7], 361863, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][8], 326159, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][9], 260541, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][10], 361863, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][11], 326159, atol=100)
 
 
 class TestPoweredEffectBinaryOnlyGuardrail(object):
@@ -807,12 +807,12 @@ class TestPoweredEffectBinaryOnlyGuardrail(object):
         assert np.isclose(powered_effect[POWERED_EFFECT][4], 0.2344, atol=0.001)
         assert np.isclose(powered_effect[POWERED_EFFECT][5], 0.2182, atol=0.001)
 
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][0], 202920, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][1], 281835, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][2], 254029, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][3], 202920, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][4], 281835, atol=100)
-        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][5], 254029, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][0], 201905, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][1], 280423, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][2], 252755, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][3], 201905, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][4], 280423, atol=100)
+        assert np.isclose(powered_effect[REQUIRED_SAMPLE_SIZE][5], 252755, atol=100)
 
 
 class TestBinary(object):
@@ -1884,19 +1884,28 @@ class TestCategoricalBinomialData(object):
         assert np.array_equal(summary.point_estimate, self.data.success / self.data.total)
         assert np.allclose(
             summary["ci_lower"],
-            np.array([0.464675, 0.309088, 0.645304, 0.401508, 0.079939, 0.590975]),
+            np.array(
+                [
+                    0.4646901340180582,
+                    0.30954466010970333,
+                    0.6453118311511006,
+                    0.4020018007729973,
+                    0.0963636851484016,
+                    0.5912276177282552,
+                ]
+            ),
             rtol=1e-3,
         )
         assert np.allclose(
             summary["ci_upper"],
             np.array(
                 [
-                    0.526405,
-                    0.498604,
-                    0.692912,
-                    0.598492,
-                    0.720061,
-                    0.742358,
+                    0.5263901434844195,
+                    0.4981476475826044,
+                    0.692903881232388,
+                    0.5979981992270027,
+                    0.7036363148515985,
+                    0.7421057156050781,
                 ]
             ),
             rtol=1e-3,
@@ -1909,32 +1918,46 @@ class TestCategoricalBinomialData(object):
         diff = self.test.multiple_difference(("us", "control"), level_as_reference=False)
         assert np.allclose(
             diff["adjusted p-value"],
-            np.array([1.000000e00, 8.396461e-01, 1.081227e-04, 3.566812e-01, 5.151362e-07]),
+            np.array([1e00, 8.291843e-01, 9.971992e-05, 3.504662e-01, 4.504966e-07]),
             rtol=1e-3,
         )
         assert np.allclose(
             diff["p-value"],
-            np.array([9.819823e-01, 1.679292e-01, 2.162454e-05, 7.133623e-02, 1.030272e-07]),
+            np.array([9.81084197e-01, 1.65836862e-01, 1.99439850e-05, 7.00932382e-02, 9.00993166e-08]),
             rtol=1e-3,
         )
         assert np.allclose(
             diff["adjusted ci_lower"],
-            np.array([-0.434833, -0.275774, -0.422207, -0.222667, -0.393664]),
+            np.array([-0.41400184, -0.27489017, -0.42153065, -0.22209041, -0.39307973]),
             rtol=1e-3,
         )
         assert np.allclose(
             diff["adjusted ci_upper"],
-            np.array([0.442525, 0.083466, -0.103434, 0.039279, -0.136859]),
+            np.array([0.42169415, 0.08258247, -0.10411038, 0.03870244, -0.13744367]),
             rtol=1e-3,
         )
 
         diff = self.test.multiple_difference("test", groupby="country", level_as_reference=False)
-        assert np.allclose(diff["adjusted p-value"], np.array([1.0000, 3.417856e-02, 2.853449e-01, 0.000]), rtol=1e-3)
-        assert np.allclose(diff["p-value"], np.array([5.583549e-01, 8.544640e-03, 7.133623e-02, 0.000]), rtol=1e-3)
         assert np.allclose(
-            diff["adjusted ci_lower"], np.array([-0.326749, -0.324964, -0.035307, -0.223240]), rtol=1e-3
+            diff["adjusted p-value"],
+            np.array([1.00000000e00, 3.30302805e-02, 2.80372953e-01, 0.0]),
+            rtol=1e-3,
         )
-        assert np.allclose(diff["adjusted ci_upper"], np.array([0.526749, -0.008369, 0.218695, -0.123895]), rtol=1e-3)
+        assert np.allclose(
+            diff["p-value"],
+            np.array([5.39020329e-01, 8.25757011e-03, 7.00932382e-02, 0.0]),
+            rtol=1e-3,
+        )
+        assert np.allclose(
+            diff["adjusted ci_lower"],
+            np.array([-0.30659699, -0.32426934, -0.03474758, -0.2232184]),
+            rtol=1e-3,
+        )
+        assert np.allclose(
+            diff["adjusted ci_upper"],
+            np.array([0.50659699, -0.00906399, 0.21813554, -0.12391703]),
+            rtol=1e-3,
+        )
 
 
 class TestWithNims(object):