e8763eeb3754386d1f8b51d03f3cf85783ca1e0f
Pelle Sillrén <pers@spotify.com>
2022-08-17 09:43:14 +0200

    - Remove default value None for method_column - Format using make black

--- tests/frequentist/test_ztest_linreg.py
+++ tests/frequentist/test_ztest_linreg.py
@@ -15,8 +15,8 @@ class TestUnivariateSingleMetric(object):
         data = pd.DataFrame({"variation_name": list(map(str, d)), "y": y, "x": x})
         data = (
             data.assign(xy=y * x)
-            .assign(x2=x ** 2)
-            .assign(y2=y ** 2)
+            .assign(x2=x**2)
+            .assign(y2=y**2)
             .groupby(["variation_name"])
             .agg({"y": ["sum", "count"], "y2": "sum", "x": "sum", "x2": "sum", "xy": "sum"})
             .reset_index()
@@ -83,8 +83,8 @@ class TestUnivariateMultiMetric(object):
         data = pd.DataFrame({"variation_name": list(map(str, d)), "metric_name": m, "y": y, "x": x})
         data = (
             data.assign(xy=y * x)
-            .assign(x2=x ** 2)
-            .assign(y2=y ** 2)
+            .assign(x2=x**2)
+            .assign(y2=y**2)
             .groupby(["variation_name", "metric_name"])
             .agg({"y": ["sum", "count"], "y2": "sum", "x": "sum", "x2": "sum", "xy": "sum"})
             .reset_index()
@@ -226,7 +226,7 @@ class TestMultivariateSingleMetric(object):
         y = 0.5 * d + 0.5 * x1 + 0.5 * x2 + np.random.standard_normal(size=n)
         df = pd.DataFrame({"variation_name": list(map(str, d)), "y": y, "x1": x1, "x2": x2})
 
-        data = df.assign(y2=y ** 2).groupby(["variation_name"]).agg({"y": ["sum", "count"], "y2": "sum"}).reset_index()
+        data = df.assign(y2=y**2).groupby(["variation_name"]).agg({"y": ["sum", "count"], "y2": "sum"}).reset_index()
 
         data.columns = data.columns.map("_".join).str.strip("_")
 
@@ -332,7 +332,7 @@ class TestMultivariateMultipleMetrics(object):
         )
 
         data = (
-            df.assign(y2=y ** 2)
+            df.assign(y2=y**2)
             .groupby(["variation_name", "metric_name"])
             .agg({"y": ["sum", "count"], "y2": "sum"})
             .reset_index()
